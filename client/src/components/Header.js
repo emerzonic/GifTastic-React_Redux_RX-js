9a662de953bdfd818582158c418b76186b8161d9
Emerson Doyah <emerson.doyah@gmail.com>
2019-01-04 16:05:24 -0600

    edit navbar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -35,7 +35,7 @@ class Header extends Component {
                             <form className="col s12" onSubmit={this.onSubmit}>
                                 <div className="row">
                                     <div className="input-field col s12 m4 inline">
-                                    <label  className="white-text" htmlFor="test5">Set Results number.</label>
+                                    <label  className="label-text" htmlFor="test5">Set Results number.</label>
 
                                         <p className="range-field">
                                             <input type="range" name="numberOfResults" 
@@ -44,7 +44,7 @@ class Header extends Component {
                                     </div>
                                     <div className="input-field col s12 m6 white-text inline">
                                         <input id="gif-input" name="searchTerm" type="text" onChange={this.onChange} value={this.state.input}  className="validate"/>
-                                        <label className="white-text"  htmlFor="gif-input">Search gifs here.</label>
+                                        <label className="label-text"  htmlFor="gif-input">Search gifs here.</label>
                                     </div>
                                     <div className="input-field col s12 m2 inline">
                                         <button className="btn waves-effect waves-light" type="submit" name="action">Submit
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Navbar = () => (
     <nav>
     <div className="nav-wrapper">
-      <a href="/" className="center brand-logo">GifTify Your Day</a>
+      <a href="/" className="center brand-logo">GifTastic</a>
     </div>
   </nav>
     
